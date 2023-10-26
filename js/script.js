e9c9a85f494edad001e541ff3918f01fc8c50132
unknown <brunocesar.tenorio1@gmail.com>
2023-10-26 13:29:36 -0300

    adding section animation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import initButtonTop from "./modules/button-top.js";
+import initScrollAnimation from "./modules/scroll-animation.js";
 initButtonTop();
+initScrollAnimation();
 
 if (window.SimpleAnime) {
   new SimpleAnime();
