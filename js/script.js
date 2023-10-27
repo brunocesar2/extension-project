3a67d1373f71336cc6e66cd32b96a5a2c8628e84
unknown <brunocesar.tenorio1@gmail.com>
2023-10-27 12:00:52 -0300

    fixing footer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 import initButtonTop from "./modules/button-top.js";
-import initScrollAnimation from "./modules/scroll-animation.js";
 initButtonTop();
-initScrollAnimation();
 
 if (window.SimpleAnime) {
   new SimpleAnime();
